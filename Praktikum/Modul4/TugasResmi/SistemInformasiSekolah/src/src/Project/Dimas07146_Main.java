4fa03906d42411d464123f703595f2ec0f00c3d0
Dimas Tri Martono <72393510+dimastrim45@users.noreply.github.com>
2021-01-14 13:44:13 +0700

    Commit Modul4

---
+++ Praktikum/Modul4/TugasResmi/SistemInformasiSekolah/src/src/Project/Dimas07146_Main.java
@@ -0,0 +1,9 @@
+package Project;
+
+import View.Dimas07146_GUI;
+
+public class Dimas07146_Main {
+    public static void main(String[] args) {
+        Dimas07146_GUI gui = new Dimas07146_GUI();
+    }
+}
---
+++ Praktikum/Modul4/TugasResmi/SistemInformasiSekolah/src/src/View/Dimas07146_allobjctrl.java
@@ -0,0 +1,7 @@
+package View;
+
+import Controller.Dimas07146_GuruController;
+
+public class Dimas07146_allobjctrl {
+    public static Dimas07146_GuruController guru = new Dimas07146_GuruController();
+}
